82d5052d67a28da8b0d217e068ba7d49d6a6ad23
Akshay Kachore <akshay.kachore@xoriant.com>
2021-12-26 00:22:03 +0530

    loggers added

--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -2,6 +2,8 @@ package com.example.demo.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,22 +13,27 @@ import com.example.demo.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeService.class);
 	@Autowired
 	private EmployeeRepository repository;
 
 	public List<Employee> findAllEmployee() {
+		logger.info("EmployeeService : findAllEmployee()");
 		return repository.findAll();
 	}
 
 	public Employee saveEmployee(Employee newEmployee) {
+		logger.info("EmployeeService : saveEmployee()");
 		return repository.save(newEmployee);
 	}
 
 	public Employee findEmploueeById(Long id) {
+		logger.info("EmployeeService : findEmploueeById()");
 		return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
 	}
 
 	public Employee updateEmployee(Employee newEmployee, Long id) {
+		logger.info("EmployeeService : updateEmployee()");
 		return repository.findById(id).map(employee -> {
 			employee.setName(newEmployee.getName());
 			employee.setRole(newEmployee.getRole());
@@ -38,6 +45,7 @@ public class EmployeeService {
 	}
 
 	public void deleteEmployee(Long id) {
+		logger.info("EmployeeService : deleteEmployee()");
 		repository.deleteById(id);
 
 	}
